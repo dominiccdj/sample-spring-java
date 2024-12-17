13faea78f22af54b240d0826993b951bab6873f8
Dominik J <dominikj008@gmail.com>
2024-12-17 16:02:11 +0100

    Add files via upload

---
+++ src/main/java/com/domi/dominik/DominikApplication.java
@@ -0,0 +1,13 @@
+package com.domi.dominik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DominikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DominikApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/domi/dominik/DominikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.domi.dominik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DominikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
